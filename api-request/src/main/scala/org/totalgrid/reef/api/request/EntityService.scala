e58758bc69bd77634dfd5029e97962619807d977
devans <devans@greenenergycorp.com>
2011-03-21 14:57:15 -0400

    JavaDoc added to entity api

--- api-request/src/main/scala/org/totalgrid/reef/api/request/EntityService.scala
+++ api-request/src/main/scala/org/totalgrid/reef/api/request/EntityService.scala
@@ -21,27 +21,110 @@
 package org.totalgrid.reef.api.request
 
 import org.totalgrid.reef.proto.Model.{ Entity, EntityAttributes }
+import org.totalgrid.reef.api.ReefServiceException
 
 trait EntityService {
+
+
+  /**
+   * Get an entity using its unique identification.
+   * @param uid   The entity id.
+   * @return The entity object.
+   */
+  @throws(classOf[ReefServiceException])
   def getEntityByUid(uid: ReefUUID): Entity
 
+  /**
+   * Get an entity using its name.
+   * @param name   The configured name of the entity.
+   * @return The entity object.
+   */
+  @throws(classOf[ReefServiceException])
   def getEntityByName(name: String): Entity
 
+  /**
+   * Find all entities with a specified type.
+   * @param typ   The entity type to search for.
+   * @return The list of entities that have the specified type.
+   */
+  @throws(classOf[ReefServiceException])
   def getAllEntitiesWithType(typ: String): java.util.List[Entity]
 
+  /**
+   * Get the attributes for a specified Entity.
+   * @param uid   The entity id.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def getEntityAttributes(uid: ReefUUID): EntityAttributes
 
+
+  /**
+   * Remove a specific attribute by name for a particular Entity.
+   * @param uid   The entity id.
+   * @param attrName    The name of the attribute.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def removeEntityAttribute(uid: ReefUUID, attrName: String): EntityAttributes
 
+
+  /**
+   * Clear all attributes for a specified Entity.
+   * @param uid   The entity id.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def clearEntityAttributes(uid: ReefUUID): EntityAttributes
 
+
+  /**
+   * Set a boolean attribute by name for a specified Entity.
+   * @param uid     The entity id.
+   * @param name    The name of the attribute.
+   * @param value   The attribute value.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def setEntityAttribute(uid: ReefUUID, name: String, value: Boolean): EntityAttributes
 
+  /**
+   * Set a signed 64-bit integer attribute by name for a specified Entity.
+   * @param uid     The entity id.
+   * @param name    The name of the attribute.
+   * @param value   The attribute value.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def setEntityAttribute(uid: ReefUUID, name: String, value: Long): EntityAttributes
 
+  /**
+   * Set an attribute of type double by name for a specified Entity.
+   * @param uid     The entity id.
+   * @param name    The name of the attribute.
+   * @param value   The attribute value.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def setEntityAttribute(uid: ReefUUID, name: String, value: Double): EntityAttributes
 
+  /**
+   * Set a string attribute by name for a specified Entity.
+   * @param uid     The entity id.
+   * @param name    The name of the attribute.
+   * @param value   The attribute value.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def setEntityAttribute(uid: ReefUUID, name: String, value: String): EntityAttributes
 
+  /**
+   * Set an attribute of type Array<Byte> by name for a specified Entity.
+   * @param uid     The entity id.
+   * @param name    The name of the attribute.
+   * @param value   The attribute value.
+   * @return The entity and its associated attributes.
+   */
+  @throws(classOf[ReefServiceException])
   def setEntityAttribute(uid: ReefUUID, name: String, value: Array[Byte]): EntityAttributes
 }
