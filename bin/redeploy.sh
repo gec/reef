40fa7c91a6ce4eac3dfb4227be7485ce7a11ec4e
devans <devans@greenenergycorp.com>
2011-05-25 17:51:41 -0400

    Updated install script name in redeploy

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -7,4 +7,4 @@ $reefdir/bin/stop > /dev/null 2>&1 || true
 rm -rf $reefdir
 tar -xvf assembly/target/totalgrid-reef*.tar.gz
 
-cd $reefdir && ./install_reef.sh samples/integration/config.xml
+cd $reefdir && ./install_config.sh samples/integration/config.xml
