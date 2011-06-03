417475b5d5d1eba37bf22316f45ed7d8d31aeec0
samhendley <shendley@greenenergycorp.com>
2011-06-03 16:38:15 -0400

    Added tests to check that comms_up/comms_down callbacks work

--- protocol-dnp3/src/test/scala/org/totalgrid/reef/protocol/dnp3/DNP3BindingTest.scala
+++ protocol-dnp3/src/test/scala/org/totalgrid/reef/protocol/dnp3/DNP3BindingTest.scala
@@ -18,17 +18,41 @@
  */
 package org.totalgrid.reef.protocol.dnp3
 
-import org.totalgrid.reef.proto.{ Mapping, Measurements }
-
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 
-@RunWith(classOf[JUnitRunner]) //disabled because it hangs under eclipse
+@RunWith(classOf[JUnitRunner])
 class DNP3BindingTest extends FunSuite with ShouldMatchers {
 
   val startPort = 32323
+
+  class MockStateObserver {
+    import scala.collection.mutable.{ Queue, Map }
+    val states = Map.empty[String, Queue[MasterStates]]
+
+    var observers = List.empty[IMasterObserver]
+    def getObserver(name: String): IMasterObserver = {
+      val obs = new IMasterObserver() {
+        override def OnStateChange(state: MasterStates) {
+          states.get(name) match {
+            case Some(l: Queue[MasterStates]) => l.enqueue(state)
+            case None => states += name -> Queue(state)
+          }
+        }
+      }
+      // need to keep a reference to the observer so it doesn't get GCed
+      observers ::= obs
+      obs
+    }
+
+    def checkStates(names: List[String], expected: List[MasterStates]) {
+      names.foreach { name =>
+        states.get(name).map { _.toList }.getOrElse(List.empty[MasterStates]) should equal(expected)
+      }
+    }
+  }
 
   /// This test shows that the startup/teardown behavior is working without crashing
   test("StartupTeardownOnJVM") {
@@ -36,7 +60,9 @@ class DNP3BindingTest extends FunSuite with ShouldMatchers {
     val num_stack = 10
     val sm = new StackManager(true) // the stack will start running as soon as a master is added		
 
-    val cfg = new MasterStackConfig
+    val stateObserver = new MockStateObserver
+
+    var names = List.empty[String]
 
     // startup <num_stack> masters on <num_port> ports
     (1 to num_port).foreach { port =>
@@ -46,15 +72,27 @@ class DNP3BindingTest extends FunSuite with ShouldMatchers {
 
       (1 to num_stack).foreach { stack =>
         val name = "port-" + port + "-stack" + stack
+
+        names ::= name
+
+        val cfg = new MasterStackConfig
+
         cfg.getLink.setLocalAddr(stack)
+
+        cfg.getMaster.setMpObserver(stateObserver.getObserver(name))
 
         // the masters won't get any data, so setting the IPublisher to null is OK
         sm.AddMaster(port.toString, name, FilterLevel.LEV_WARNING, null, cfg)
       }
     }
 
+    stateObserver.checkStates(names, List(MasterStates.MS_COMMS_DOWN))
+
     // the manager is already running at this point, so let's stop it explicitly
     sm.Stop
+
+    // make sure we didnt get a second update
+    stateObserver.checkStates(names, List(MasterStates.MS_COMMS_DOWN))
   }
 
   test("MasterToSlaveOnJVM") {
@@ -64,6 +102,9 @@ class DNP3BindingTest extends FunSuite with ShouldMatchers {
     val lev = FilterLevel.LEV_WARNING
     val sm = new StackManager(false)
     val a = new CountingPublisherActor
+
+    val stateObserver = new MockStateObserver
+    var names = List.empty[String]
 
     val master = new MasterStackConfig
     master.getMaster.setIntegrityRate(60000)
@@ -78,6 +119,9 @@ class DNP3BindingTest extends FunSuite with ShouldMatchers {
       sm.AddTCPClient(client, s, "127.0.0.1", port)
       sm.AddTCPServer(server, s, "0.0.0.0", port)
 
+      master.getMaster.setMpObserver(stateObserver.getObserver(server))
+      names ::= server
+
       sm.AddMaster(client, client, lev, a.addPub, master)
       sm.AddSlave(server, server, lev, null, slave)
     }
@@ -85,6 +129,9 @@ class DNP3BindingTest extends FunSuite with ShouldMatchers {
     sm.Start
     assert(a.waitForMinMessages(300, 10000))
     sm.Stop
+
+    // make sure we got the down-up-down callbacks we expected
+    stateObserver.checkStates(names, List(MasterStates.MS_COMMS_DOWN, MasterStates.MS_COMMS_UP, MasterStates.MS_COMMS_DOWN))
   }
 
 }
