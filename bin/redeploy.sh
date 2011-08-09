1f07b36834fa420079772c03b8b6881d3149271f
devans <devans@greenenergycorp.com>
2011-08-09 15:04:16 -0400

    Fixed redeploy for new assembly

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -5,9 +5,6 @@ reefdir=totalgrid-reef-*
 $reefdir/bin/stop > /dev/null 2>&1 || true
 
 rm -rf $reefdir
-mkdir -p assembly/target/temp
-mv assembly/target/totalgrid-reef-console* assembly/target/temp
-tar -xvf assembly/target/totalgrid-reef*.tar.gz
-mv assembly/target/temp/* assembly/target
+tar -xvf assemblies/assembly/target/totalgrid-reef*.tar.gz
 
 cd $reefdir && ./install_config.sh samples/integration/config.xml
