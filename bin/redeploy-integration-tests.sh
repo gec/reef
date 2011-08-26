a4353b740af239321b5bc7cce078314988576e1b
Patrick Osborne <posborne@greenenergycorp.com>
2011-08-26 14:07:30 -0600

    new redeploy script for integration testing.  this script installs the integration testing model in assembly-common.  this script is used by the TeamCity Reef integration build configuration.

---
+++ bin/redeploy-integration-tests.sh
@@ -0,0 +1,10 @@
+set -ex
+
+reefdir=totalgrid-reef-*
+
+$reefdir/bin/stop > /dev/null 2>&1 || true
+
+rm -rf $reefdir
+tar -xvf assemblies/assembly/target/totalgrid-reef*.tar.gz
+
+cd $reefdir && ./install_config.sh samples/integration/config.xml
