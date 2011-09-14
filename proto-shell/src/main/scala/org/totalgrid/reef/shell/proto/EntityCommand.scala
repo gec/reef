e352e08d9a2e17499a3101313248aaa69fc247c3
samhendley <shendley@greenenergycorp.com>
2011-09-14 10:20:28 -0400

    Added an entity:tree command to prove out entity tree service calls

--- proto-shell/src/main/scala/org/totalgrid/reef/shell/proto/EntityCommand.scala
+++ proto-shell/src/main/scala/org/totalgrid/reef/shell/proto/EntityCommand.scala
@@ -78,3 +78,42 @@ class EntityChildrenCommand extends ReefCommandSupport {
   }
 }
 
+@Command(scope = "entity", name = "tree", description = "Prints all entities or information on a specific entity.")
+class EntityTreeCommand extends ReefCommandSupport {
+
+  @Argument(index = 0, name = "Entity Name", description = "Entity name.", required = false, multiValued = false)
+  var rootName: String = null
+
+  @Argument(index = 1, name = "Relationship", description = "Name of relationship type", required = false, multiValued = false)
+  var relationship: String = null
+
+  @Argument(index = 2, name = "types", description = "List of types we want in returned list", required = false, multiValued = true)
+  var types: java.util.List[String] = null
+
+  def doCommand() = {
+    val rootEntityName = Option(rootName) match {
+      case Some(entId) => entId
+      case None => getRootName.getOrElse(throw new Exception("No root entity configured, use entity:tree <name> to root tree!"))
+    }
+    val rel = Option(relationship).getOrElse(getRelationship.getOrElse("owns"))
+
+    val rootEntity = services.getEntityByName(rootEntityName)
+
+    // store the current options
+    setRootName(rootEntityName)
+    setRelationship(rel)
+
+    val ents = Option(types) match {
+      case Some(typs) => services.getEntityImmediateChildren(rootEntity.getUuid, rel, typs)
+      case None => services.getEntityImmediateChildren(rootEntity.getUuid, rel)
+    }
+    EntityView.printTreeSingleDepth(rootEntity, ents.toList)
+  }
+
+  private def getRootName: Option[String] = get("entity:tree:root")
+  private def setRootName(name: String) = set("entity:tree:root", name)
+
+  private def getRelationship: Option[String] = get("entity:tree:rel")
+  private def setRelationship(rel: String) = set("entity:tree:rel", rel)
+}
+
