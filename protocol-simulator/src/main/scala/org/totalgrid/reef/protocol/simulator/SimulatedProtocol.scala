71d3b8bfd241305ddeed45fefdf9fb4c90e37296
samhendley <shendley@greenenergycorp.com>
2011-10-20 17:00:29 -0400

    temporary change of api proto name until loader is available in 0.4.x

--- protocol-simulator/src/main/scala/org/totalgrid/reef/protocol/simulator/SimulatedProtocol.scala
+++ protocol-simulator/src/main/scala/org/totalgrid/reef/protocol/simulator/SimulatedProtocol.scala
@@ -54,7 +54,8 @@ class SimulatedProtocol(exe: Executor) extends LoggingProtocolEndpointManager wi
       case Some(x) =>
         throw new IllegalStateException("Endpoint has already been added: " + endpoint)
       case None =>
-        val file = Protocol.find(files, "application/vnd.google.protobuf; proto=reef.api.proto.SimMapping.SimulatorMapping").getFile
+        // TODO: fix proto mimetype once loader is back up
+        val file = Protocol.find(files, "application/vnd.google.protobuf; proto=reef.proto.SimMapping.SimulatorMapping").getFile
         val mapping = SimMapping.SimulatorMapping.parseFrom(file)
         val emptyRecord = new PluginRecord(endpoint, mapping, batchPublisher, None)
         endpoints += endpoint -> emptyRecord
