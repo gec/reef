55f15229d1e81329e9e8cf70bc0ff17be0ee6ccd
samhendley <shendley@greenenergycorp.com>
2011-11-04 13:56:29 -0400

    Added entrypoint for running services directly from IDE

---
+++ services/src/main/scala/org/totalgrid/reef/services/activator/ServiceEntryPoint.scala
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2011 Green Energy Corp.
+ *
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Green Energy
+ * Corp licenses this file to you under the GNU Affero General Public License
+ * Version 3.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/agpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.totalgrid.reef.services.activator
+
+import org.totalgrid.reef.services.ServiceOptions
+import org.totalgrid.reef.clientapi.settings.{ NodeSettings, UserSettings, AmqpSettings }
+import org.totalgrid.reef.app.ConnectionCloseManagerEx
+import org.totalgrid.reef.clientapi.settings.util.PropertyReader
+import net.agileautomata.executor4s.Executors
+import java.util.concurrent.{ Executors => JExecutors }
+import org.totalgrid.reef.util.ShutdownHook
+import org.totalgrid.reef.persistence.squeryl.{ DbConnector, DbInfo }
+import org.totalgrid.reef.measurementstore.squeryl.SqlMeasurementStore
+import org.totalgrid.reef.clientapi.sapi.service.AsyncService
+
+object ServiceEntryPoint extends ShutdownHook {
+  def main(args: Array[String]) = {
+    val scheduler = JExecutors.newScheduledThreadPool(Runtime.getRuntime.availableProcessors())
+    val executor = JExecutors.newCachedThreadPool()
+
+    val exe = Executors.newCustomExecutor(executor, scheduler)
+
+    val brokerConfig = new AmqpSettings(PropertyReader.readFromFile("org.totalgrid.reef.amqp.cfg"))
+    val sql = new DbInfo(PropertyReader.readFromFile("org.totalgrid.reef.sql.cfg"))
+    val options = new ServiceOptions(PropertyReader.readFromFile("org.totalgrid.reef.services.cfg"))
+    val userSettings = new UserSettings(PropertyReader.readFromFile("org.totalgrid.reef.user.cfg"))
+    val nodeSettings = new NodeSettings(PropertyReader.readFromFile("org.totalgrid.reef.node.cfg"))
+
+    val manager = new ConnectionCloseManagerEx(brokerConfig, exe)
+
+    val modules = new ServiceModulesFactory {
+      def getDbConnector() = DbConnector.connect(sql)
+      def getMeasStore() = SqlMeasurementStore
+      def publishServices(services: Seq[AsyncService[_]]) = {}
+    }
+
+    manager.addConsumer(ServiceFactory.create(options, userSettings, nodeSettings, modules))
+
+    manager.start()
+
+    waitForShutdown {
+      manager.stop()
+    }
+  }
+}
---
+++ services/src/main/scala/org/totalgrid/reef/services/activator/ServiceFactory.scala
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2011 Green Energy Corp.
+ *
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Green Energy
+ * Corp licenses this file to you under the GNU Affero General Public License
+ * Version 3.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/agpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.totalgrid.reef.services.activator
+
+import org.totalgrid.reef.clientapi.settings.{ NodeSettings, UserSettings }
+import org.totalgrid.reef.app.ConnectionConsumer
+import org.totalgrid.reef.broker.BrokerConnection
+import net.agileautomata.executor4s.Executor
+import org.totalgrid.reef.client.sapi.ReefServices
+import org.totalgrid.reef.metrics.MetricsSink
+import org.totalgrid.reef.services.authz.SqlAuthzService
+import org.totalgrid.reef.services.{ ServiceContext, ServiceProviders, ServiceBootstrap, ServiceOptions }
+import org.totalgrid.reef.util.{ Cancelable, LifecycleManager }
+import org.totalgrid.reef.measurementstore.MeasurementStore
+import org.totalgrid.reef.clientapi.sapi.service.AsyncService
+
+/**
+ * gets other modules used by the services so can implemented via OSGI or directly
+ */
+trait ServiceModulesFactory {
+  def getDbConnector(): Unit
+
+  def getMeasStore(): MeasurementStore
+
+  def publishServices(services: Seq[AsyncService[_]])
+}
+
+object ServiceFactory {
+  def create(serviceOptions: ServiceOptions, userSettings: UserSettings, nodeSettings: NodeSettings, modules: ServiceModulesFactory) = {
+    new ConnectionConsumer {
+      def newConnection(brokerConnection: BrokerConnection, exe: Executor) = {
+
+        val connection = ReefServices(brokerConnection, exe)
+
+        modules.getDbConnector()
+
+        val (appConfig, authToken) = ServiceBootstrap.bootstrapComponents(connection, userSettings, nodeSettings)
+
+        val metricsHolder = MetricsSink.getInstance(appConfig.getInstanceName)
+
+        val mgr = new LifecycleManager
+        val measStore = modules.getMeasStore()
+
+        val providers = new ServiceProviders(connection, measStore, serviceOptions, SqlAuthzService, metricsHolder, authToken)
+
+        val serviceContext = new ServiceContext(connection, exe)
+
+        serviceContext.addCoordinator(providers.coordinators)
+
+        val services = serviceContext.attachServices(providers.services)
+
+        modules.publishServices(services)
+
+        mgr.start()
+
+        new Cancelable {
+          def cancel() = {
+            providers.coordinators.foreach { _.stopProcess() }
+            mgr.stop()
+          }
+        }
+      }
+    }
+  }
+}
