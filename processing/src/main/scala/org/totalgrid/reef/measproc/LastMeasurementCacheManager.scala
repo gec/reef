fd12dd05cb06edb11af4e2248120a88935e88ae8
samhendley <shendley@greenenergycorp.com>
2012-01-24 11:25:37 -0500

    Fixed endpointConnection subscription by endpoint name or uuid

--- processing/src/main/scala/org/totalgrid/reef/measproc/LastMeasurementCacheManager.scala
+++ processing/src/main/scala/org/totalgrid/reef/measproc/LastMeasurementCacheManager.scala
@@ -22,6 +22,7 @@ import org.totalgrid.reef.client.service.proto.FEP.EndpointConnection
 import scala.collection.immutable
 import org.totalgrid.reef.client.service.proto.Measurements.Measurement
 import org.totalgrid.reef.persistence.{ ObjectCache, InMemoryObjectCache }
+import com.weiglewilczek.slf4s.Logging
 
 object LastMeasurementCacheManager {
   class ResettableInMemoryObjectCache[A] extends InMemoryObjectCache[A] {
@@ -31,18 +32,23 @@ object LastMeasurementCacheManager {
   }
 }
 
-class LastMeasurementCacheManager(endpoint: String) extends MeasProcServiceContext[EndpointConnection] {
+class LastMeasurementCacheManager(endpoint: String) extends MeasProcServiceContext[EndpointConnection] with Logging {
   import LastMeasurementCacheManager._
 
-  def add(obj: EndpointConnection) = {}
-  def remove(obj: EndpointConnection) = {}
-  def clear() = {}
+  // we may see add/remove events if the protocol is changed
+  def add(end: EndpointConnection) = modify(end)
+  def remove(end: EndpointConnection) = modify(end)
+  def clear() = objCache.reset()
 
   private val objCache = new ResettableInMemoryObjectCache[Measurement]
   def cache: ObjectCache[Measurement] = objCache
 
   override def modify(end: EndpointConnection) = {
-    if (end.getEndpoint.getName == endpoint && (end.getEnabled == false || end.getState == EndpointConnection.State.COMMS_DOWN)) {
+    if (end.getEndpoint.getName != endpoint) {
+      logger.warn("Endpoint didn't have expected name: " + end.getEndpoint.getName + " instead of: " + endpoint)
+    } else {
+      logger.debug("Resetting measurement filter for: " + endpoint)
+      // agressivley reset the cache, a few extra measurements in the store is not a problem
       objCache.reset()
     }
   }
