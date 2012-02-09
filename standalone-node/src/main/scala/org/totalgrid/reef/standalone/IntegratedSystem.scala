345477111026a74118e535e19e3bf5b87072c58a
samhendley <shendley@greenenergycorp.com>
2012-02-09 12:34:05 -0500

    Updated integrated system to use ConnectedApplications

--- standalone-node/src/main/scala/org/totalgrid/reef/standalone/IntegratedSystem.scala
+++ standalone-node/src/main/scala/org/totalgrid/reef/standalone/IntegratedSystem.scala
@@ -24,8 +24,6 @@ import org.totalgrid.reef.client.sapi.service.AsyncService
 import org.totalgrid.reef.services.{ ServiceBootstrap, ServiceOptions }
 import net.agileautomata.executor4s._
 import org.totalgrid.reef.services.activator.{ ServiceFactory, ServiceModulesFactory }
-import org.totalgrid.reef.measproc.activator.ProcessingActivator
-import org.totalgrid.reef.entry.FepEntry
 import org.totalgrid.reef.client.sapi.client.rest.impl.DefaultConnection
 import org.totalgrid.reef.client.service.list.ReefServices
 import org.totalgrid.reef.shell.proto.ProtoShellApplication
@@ -34,6 +32,10 @@ import org.totalgrid.reef.loader.commons.{ LoaderServices, LoaderServicesList }
 import org.totalgrid.reef.client.settings.util.{ PropertyLoading, PropertyReader }
 import com.weiglewilczek.slf4s.Logging
 import org.totalgrid.reef.models.CoreServicesSchema
+import org.totalgrid.reef.app.impl.{ ApplicationManagerSettings, SimpleConnectedApplicationManager }
+import net.agileautomata.executor4s.testing.InstantExecutor
+import org.totalgrid.reef.measproc.activator.MeasurementProcessorConnectedApplication
+import org.totalgrid.reef.entry.FepConnectedApplication
 
 class IntegratedSystem(exe: Executor, configFile: String, resetFirst: Boolean) extends Logging {
 
@@ -72,13 +74,17 @@ class IntegratedSystem(exe: Executor, configFile: String, resetFirst: Boolean) e
 
   nodeSettings.foreach { nodeSettings =>
 
+    val appManagerSettings = new ApplicationManagerSettings(userSettings, nodeSettings)
+    val applicationManager = new SimpleConnectedApplicationManager(new InstantExecutor, manager, appManagerSettings)
+    applicationManager.start()
+
     manager.addConsumer(ServiceFactory.create(options, userSettings, nodeSettings, modules))
 
-    manager.addConsumer(ProcessingActivator.createMeasProcessor(userSettings, nodeSettings, measurementStore))
+    applicationManager.addConnectedApplication(new MeasurementProcessorConnectedApplication(measurementStore))
 
     // we need to load the protocol separately for each node
     loadProtocols(properties, exe).foreach { protocol =>
-      manager.addConsumer(FepEntry.createFepConsumer(userSettings, nodeSettings, protocol))
+      applicationManager.addConnectedApplication(new FepConnectedApplication(protocol))
     }
   }
 
