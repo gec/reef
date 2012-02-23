f9ecc3ead31e574c172091c5e2b70c82b63d39d5
devans <devans@greenenergycorp.com>
2012-02-23 16:02:09 -0500

    Working simple spike on arithmetic parser

--- calculations/calculations-library/src/test/scala/org/totalgrid/reef/calc/lib/parse/OperationParserTest.scala
+++ calculations/calculations-library/src/test/scala/org/totalgrid/reef/calc/lib/parse/OperationParserTest.scala
@@ -16,30 +16,33 @@ object DumbMathParser extends JavaTokenParsers {
 }
 
 sealed trait Node
-case class Op(op: String, left: Node, right: Node) extends Node
-case class Value(v: Double)
-
-case class Mark(mk: String, rest: Any)
+case class Op(op: String, left: Node, right: Node) extends Node {
+  override def toString() = "(" + left + " " + op + " " + right + ")"
+}
+case class Value(v: Double) extends Node {
+  override def toString() = v.toString
+}
+case class Part(op: String, right: Node)
 
 object MathParser extends JavaTokenParsers {
 
-  def math: Parser[Any] = term ~ rep("+" ~ term ^^ mark("+") | "-" ~ term) ^^ mark("term")
-  def term: Parser[Any] = factor ~ rep("*" ~ factor ^^ mark("*") | "/" ~ factor)
-  def factor: Parser[Any] = floatingPointNumber ^^ {x => Value(x.toDouble)}  | "(" ~> math <~ ")"
+  def math: Parser[Node] = term ~ rep("+" ~ term ^^ part | "-" ~ term ^^ part) ^^ opList
+  def term: Parser[Node] = factor ~ rep("*" ~ factor ^^ part | "/" ~ factor ^^ part) ^^ opList
+  def factor: Parser[Node] = floatingPointNumber ^^ {x => Value(x.toDouble)}  | "(" ~> math <~ ")"
 
-  def mark(mk: String): Any => Mark = {
-    case x => 
-      println(mk + ": " + x)
-      Mark(mk, x)
+  def part: Any => Part = {
+    case (op: String) ~ (r: Node) => Part(op, r)
   }
-  /*
-  def math: Parser[Any] = term ~ rep("+" ~ term | "-" ~ term)
-  def term: Parser[Any] = factor ~ rep("*" ~ factor | "/" ~ factor)
-  def factor: Parser[Any] = floatingPointNumber  | "(" ~ math ~ ")"
-   */
-  
-  def parse(str: String) = parseAll(math, str)
 
+  def opList: ~[Node, List[Part]] => Node = {
+    case n ~ Nil => n
+    case l ~ List(Part(op, r)) => Op(op, l, r)
+    case l ~ list /*(list: List[Part])*/ => list.foldLeft(l) { case (l, part) => Op(part.op, l, part.right) }
+    case x =>
+      throw new Exception("transform failure: " + x)
+  }
+
+  def parse(str: String) = parseAll(math, str)
 }
 
 @RunWith(classOf[JUnitRunner])
@@ -47,8 +50,9 @@ class OperationParserTest extends FunSuite with ShouldMatchers {
 
   test("ParseMath") {
 
-    val mathStr = "(5 * 3) + 2"
-    val mathStr2 = "5 * 3 + 2"
+    val mathStr = "(5 + 3) * 2"
+    val mathStr2 = "5 + 3 * 2"
+    val mathStr3 = "5 + 3 * 2 / 10"
 
     val superDumb = "8 * 4"
 
@@ -57,15 +61,15 @@ class OperationParserTest extends FunSuite with ShouldMatchers {
 
     println("")
 
+    println("")
+
     println(MathParser.parse(mathStr))
+    println("")
     println(MathParser.parse(mathStr2))
-
-
+    println("")
     println(MathParser.parse(superDumb))
+    println("")
+    println(MathParser.parse(mathStr3))
   }
-
-  /*test("ParseTest") {
-    OperationParser.test()
-  }*/
 
 }
