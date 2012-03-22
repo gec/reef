f7469ba695a0c57eabfca596b133426ecb49a373
samhendley <shendley@greenenergycorp.com>
2012-03-22 12:53:56 -0400

    formatted auth token service

--- services/src/main/scala/org/totalgrid/reef/services/core/AuthTokenService.scala
+++ services/src/main/scala/org/totalgrid/reef/services/core/AuthTokenService.scala
@@ -136,79 +136,77 @@ class AuthTokenServiceModel
     with AuthTokenConversions
     with ServiceModelSystemEventPublisher {
 
-  override def createFromProto(context: RequestContext, authToken: AuthToken): AuthTokenModel =
-    {
-      logger.info("logging in agent: " + authToken.getAgent.getName)
-      val currentTime: Long = System.currentTimeMillis // need one time for authToken DB entry and posted event
+  override def createFromProto(context: RequestContext, authToken: AuthToken): AuthTokenModel = {
+    logger.info("logging in agent: " + authToken.getAgent.getName)
+    val currentTime: Long = System.currentTimeMillis // need one time for authToken DB entry and posted event
 
-      val agentName: String = authToken.agent.name.getOrElse(postLoginException(context, "", Status.BAD_REQUEST, "Cannot login without setting agent name."))
+    val agentName: String = authToken.agent.name.getOrElse(postLoginException(context, "", Status.BAD_REQUEST, "Cannot login without setting agent name."))
 
-      // check the password, PUNT: maybe replace this with a nonce + MD5 or something better
-      val agentRecord: Option[Agent] = AgentConversions.findRecord(context, authToken.getAgent)
-      agentRecord match {
-        case None =>
-          logger.info("unable to find agent: " + authToken.getAgent)
+    // check the password, PUNT: maybe replace this with a nonce + MD5 or something better
+    val agentRecord: Option[Agent] = AgentConversions.findRecord(context, authToken.getAgent)
+    agentRecord match {
+      case None =>
+        logger.info("unable to find agent: " + authToken.getAgent)
+        postLoginException(context, agentName, Status.UNAUTHORIZED, "Invalid agent or password")
+
+      case Some(agent) =>
+        if (!agent.checkPassword(authToken.getAgent.getPassword)) {
           postLoginException(context, agentName, Status.UNAUTHORIZED, "Invalid agent or password")
+        }
+        context.set("agent", agent)
+        processLogin(context, agent, authToken, currentTime)
+    }
+  }
 
-        case Some(agent) =>
-          if (!agent.checkPassword(authToken.getAgent.getPassword)) {
-            postLoginException(context, agentName, Status.UNAUTHORIZED, "Invalid agent or password")
-          }
-          context.set("agent", agent)
-          processLogin(context, agent, authToken, currentTime)
+  private def processLogin(context: RequestContext, agent: Agent, authToken: AuthToken, currentTime: Long): AuthTokenModel = {
+
+    val availableSets = agent.permissionSets.value.toList // permissions we can have
+    // permissions we are asking for allow the user to request either all of their permission sets or just a subset, barf if they
+    // ask for permisions they dont have
+    val permissionsRequested = authToken.getPermissionSetsList.toList
+    val setQuerySize = permissionsRequested.map(ps => PermissionSetConversions.searchQuerySize(ps)).sum
+    val permissionSets = if (setQuerySize > 0) {
+      val askedForSets = permissionsRequested.map(ps => PermissionSetConversions.findRecords(context, ps)).flatten.distinct
+      val unavailableSets = askedForSets.diff(availableSets)
+      if (unavailableSets.size > 0) {
+        postLoginException(context, agent.entityName, Status.UNAUTHORIZED, "No access to permission sets: " + unavailableSets)
       }
+      askedForSets
+    } else {
+      availableSets
     }
 
-  def processLogin(context: RequestContext, agent: Agent, authToken: AuthToken, currentTime: Long): AuthTokenModel =
-    {
-
-      val availableSets = agent.permissionSets.value.toList // permissions we can have
-      // permissions we are asking for allow the user to request either all of their permission sets or just a subset, barf if they
-      // ask for permisions they dont have
-      val permissionsRequested = authToken.getPermissionSetsList.toList
-      val setQuerySize = permissionsRequested.map(ps => PermissionSetConversions.searchQuerySize(ps)).sum
-      val permissionSets = if (setQuerySize > 0) {
-        val askedForSets = permissionsRequested.map(ps => PermissionSetConversions.findRecords(context, ps)).flatten.distinct
-        val unavailableSets = askedForSets.diff(availableSets)
-        if (unavailableSets.size > 0) {
-          postLoginException(context, agent.entityName, Status.UNAUTHORIZED, "No access to permission sets: " + unavailableSets)
-        }
-        askedForSets
-      } else {
-        availableSets
+    // allow the user to set the expiration time explicitly or use the default from the most restrictive permissionset
+    val expirationTime = if (authToken.hasExpirationTime) {
+      val time = authToken.getExpirationTime
+      if (time <= currentTime) {
+        postLoginException(context, agent.entityName, Status.BAD_REQUEST, "Expiration time cannot be in the past")
       }
-
-      // allow the user to set the expiration time explicitly or use the default from the most restrictive permissionset
-      val expirationTime = if (authToken.hasExpirationTime) {
-        val time = authToken.getExpirationTime
-        if (time <= currentTime) {
-          postLoginException(context, agent.entityName, Status.BAD_REQUEST, "Expiration time cannot be in the past")
-        }
-        time
-      } else {
-        // one month
-        currentTime + 18144000000L
-      }
-
-      // For now, just warn if the client version is unknown
-      val version = if (authToken.hasClientVersion) {
-        authToken.getClientVersion
-      } else {
-        logger.warn("Client attempting to login with unknown version")
-        "Unknown"
-      }
-
-      // TODO: generate an unguessable security token
-      val token = java.util.UUID.randomUUID().toString
-      val newAuthToken = table.insert(new AuthTokenModel(token, agent.id, authToken.getLoginLocation, version, expirationTime))
-      // link the token to all of the permisisonsSet they have checked out access to
-      permissionSets.foreach(ps => ApplicationSchema.tokenSetJoins.insert(new AuthTokenPermissionSetJoin(ps.id, newAuthToken.id)))
-
-      postSystemEvent(context, EventType.System.UserLogin, args = List("user" -> agent.entity.value.name))
-      newAuthToken
+      time
+    } else {
+      // one month
+      currentTime + 18144000000L
     }
 
-  def postLoginException[A](context: RequestContext, userName: String, status: Status, reason: String): A = {
+    // For now, just warn if the client version is unknown
+    val version = if (authToken.hasClientVersion) {
+      authToken.getClientVersion
+    } else {
+      logger.warn("Client attempting to login with unknown version")
+      "Unknown"
+    }
+
+    // TODO: generate an unguessable security token
+    val token = java.util.UUID.randomUUID().toString
+    val newAuthToken = table.insert(new AuthTokenModel(token, agent.id, authToken.getLoginLocation, version, expirationTime))
+    // link the token to all of the permisisonsSet they have checked out access to
+    permissionSets.foreach(ps => ApplicationSchema.tokenSetJoins.insert(new AuthTokenPermissionSetJoin(ps.id, newAuthToken.id)))
+
+    postSystemEvent(context, EventType.System.UserLogin, args = List("user" -> agent.entity.value.name))
+    newAuthToken
+  }
+
+  private def postLoginException[A](context: RequestContext, userName: String, status: Status, reason: String): A = {
     postSystemEvent(context, EventType.System.UserLoginFailure, args = "reason" -> reason :: Nil, userId = Some(userName))
     throw new BadRequestException(reason, status)
   }
@@ -242,6 +240,7 @@ class AuthTokenService(protected val model: AuthTokenServiceModel)
 
   override val descriptor = Descriptors.authToken
 
+  // we are overriding this to skip the auth step
   override def performCreate(context: RequestContext, model: ServiceModelType, request: ServiceType): ModelType = {
     model.createFromProto(context, request)
   }
