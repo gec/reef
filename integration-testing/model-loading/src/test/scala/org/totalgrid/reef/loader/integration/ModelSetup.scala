4779718261ce9d5ed742e4e27364ed4e61c8a5eb
samhendley <shendley@greenenergycorp.com>
2012-04-18 12:46:51 -0400

    Moved very slow model-loading tests to separate test project

--- integration-testing/model-loading/src/test/scala/org/totalgrid/reef/loader/integration/ModelSetup.scala
+++ integration-testing/model-loading/src/test/scala/org/totalgrid/reef/loader/integration/ModelSetup.scala
@@ -16,7 +16,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.totalgrid.reef.client.sapi.rpc.impl
+package org.totalgrid.reef.loader.integration
 
 import org.totalgrid.reef.loader.LoadManager
 
