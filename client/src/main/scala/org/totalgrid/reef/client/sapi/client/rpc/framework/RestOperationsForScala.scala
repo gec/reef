490411b41b1248f178b0739c214fa44f68c1d267
devans <devans@greenenergycorp.com>
2012-04-23 12:39:52 -0400

    Starting clean on promise refactoring

--- client/src/main/scala/org/totalgrid/reef/client/sapi/client/rpc/framework/RestOperationsForScala.scala
+++
@@ -1,9 +0,0 @@
-package org.totalgrid.reef.client.sapi.client.rpc.framework
-
-/*
-import org.totalgrid.reef.client.operations.{ RestOperations => JRestOperations}
-import org.totalgrid.reef.client.sapi.client.rest.{ RestOperations => SRestOperations }
-
-class RestOperationsForScala(javaOps: JRestOperations) extends RestOperations {
-
-}   */
