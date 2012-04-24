0111c0b8b8a622ebcf1f7400db285ebeff90a556
devans <devans@greenenergycorp.com>
2012-04-24 13:34:37 -0400

    Deleting unused interfaces from last commit

--- client/src/main/java/org/totalgrid/reef/client/Result.java
+++
@@ -1,6 +0,0 @@
-package org.totalgrid.reef.client;
-
-public interface Result<T> {
-    boolean isSuccess();
-    T get();
-}
--- client/src/main/java/org/totalgrid/reef/client/ResultTransformation.java
+++
@@ -1,5 +0,0 @@
-package org.totalgrid.reef.client;
-
-public interface ResultTransformation<T, U> {
-    Result<U> transform(T value);
-}
