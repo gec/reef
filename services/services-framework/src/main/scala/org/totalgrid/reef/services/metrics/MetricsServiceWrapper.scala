edcb2ba2b50247cc3e007a9b6221aa8d032ebe24
samhendley <shendley@greenenergycorp.com>
2012-08-08 12:01:55 -0400

    Service metrics now correctly share counters when not split

--- services/services-framework/src/main/scala/org/totalgrid/reef/services/metrics/MetricsServiceWrapper.scala
+++ services/services-framework/src/main/scala/org/totalgrid/reef/services/metrics/MetricsServiceWrapper.scala
@@ -20,7 +20,8 @@ package org.totalgrid.reef.services.metrics
 
 import org.totalgrid.reef.services.framework.ServiceEntryPoint
 import org.totalgrid.reef.services.settings.ServiceOptions
-import org.totalgrid.reef.jmx.MetricsSource
+import org.totalgrid.reef.jmx.{ Tag, MetricsSource }
+import org.totalgrid.reef.client.proto.Envelope
 
 /**
  * attaches Services to the bus but wraps the response functions with 2 pieces of "middleware".
@@ -29,21 +30,53 @@ import org.totalgrid.reef.jmx.MetricsSource
  */
 class MetricsServiceWrapper(metricsSource: MetricsSource, serviceConfiguration: ServiceOptions) {
 
+  /// when we don't keep counts indidually we need to give all the instrumeter the same set of hooks
+  private lazy val allMetrics = getHooks("all")
+
   /// binds a proto serving endpoint to the broker and depending on configuration
   /// will also instrument the call with hooks to track # and length of service requests
   def instrumentCallback[A <: AnyRef](endpoint: ServiceEntryPoint[A]): ServiceEntryPoint[A] = {
     if (serviceConfiguration.metrics) {
       val serviceName = endpoint.descriptor.id()
-      lazy val allMetrics = metricsSource.metrics("Services")
 
-      val hooks = serviceConfiguration.metricsSplitByVerb match {
-        case true => ProtoServicableMetrics.perVerbHooks(metricsSource, serviceName)
-        case false => ProtoServicableMetrics.allVerbsHooks(allMetrics, serviceName)
+      val hooks = serviceConfiguration.metricsSplitByService match {
+        case false => allMetrics
+        case true => getHooks(serviceName)
       }
 
       new ServiceMetricsInstrumenter(endpoint, hooks, serviceConfiguration.slowQueryThreshold, serviceConfiguration.chattyTransactionThreshold)
     } else {
       endpoint
+    }
+  }
+
+  private def getHooks(serviceName: String) = {
+    serviceConfiguration.metricsSplitByVerb match {
+      case true => perVerbHooks(metricsSource, serviceName)
+      case false => allVerbsHooks(metricsSource, serviceName)
+    }
+  }
+
+  private def perVerbHooks(source: MetricsSource, serviceName: String): ServiceMetricHooks = {
+    val tagList = Tag("instance", serviceName) :: Tag("type", "Service") :: Nil
+    new ServiceMetricHooks {
+      override val map = Map(
+        Envelope.Verb.GET -> new ServiceVerbHooks(source.metrics("get", tagList)),
+        Envelope.Verb.PUT -> new ServiceVerbHooks(source.metrics("put", tagList)),
+        Envelope.Verb.DELETE -> new ServiceVerbHooks(source.metrics("delete", tagList)),
+        Envelope.Verb.POST -> new ServiceVerbHooks(source.metrics("post", tagList)))
+    }
+  }
+
+  private def allVerbsHooks(source: MetricsSource, serviceName: String): ServiceMetricHooks = {
+    val tagList = Tag("instance", serviceName) :: Tag("type", "Service") :: Nil
+    val hooks = new ServiceVerbHooks(source.metrics("all", tagList))
+    new ServiceMetricHooks {
+      override val map = Map(
+        Envelope.Verb.GET -> hooks,
+        Envelope.Verb.PUT -> hooks,
+        Envelope.Verb.DELETE -> hooks,
+        Envelope.Verb.POST -> hooks)
     }
   }
 }
